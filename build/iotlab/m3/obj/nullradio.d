2a44836f961bc9cdb0ad76d06ef906b1ee0b8f1a
Wifi2023stras10 Wifi2023stras10 <wifi2023stras10@srvssh>
2023-04-18 18:19:09 +0200

    clean

--- build/iotlab/m3/obj/nullradio.d
+++
@@ -1,9 +0,0 @@
-build/iotlab/m3/obj/nullradio.o: \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/dev/nullradio.c \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/services/shell/module-macros.h \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/dev/nullradio.h \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/dev/radio.h
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/dev/nullradio.c :
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/services/shell/module-macros.h :
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/dev/nullradio.h :
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/dev/radio.h :
--- build/iotlab/m3/obj/stm32f1xx/boot.d
+++
@@ -1,7 +0,0 @@
-build/iotlab/m3/obj/stm32f1xx/boot.o: \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/arch//platform/iotlab/openlab/drivers/stm32f1xx/boot.c \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/services/shell/module-macros.h \
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/arch//platform/iotlab/openlab/drivers/cortex-m3/boot.h
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/arch//platform/iotlab/openlab/drivers/stm32f1xx/boot.c :
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/contiki-ng/os/services/shell/module-macros.h :
- /senslab/users/wifi2023stras10/iot-lab/parts/iot-lab-contiki-ng/arch//platform/iotlab/openlab/drivers/cortex-m3/boot.h :
