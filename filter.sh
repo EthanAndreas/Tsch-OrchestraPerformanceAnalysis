33aa4d3d716f55ef8547a62c7b145ea0fb1e70a0
EthanAndreas <ethanhuret27@gmail.com>
2023-04-27 14:56:23 +0200

    try to launch multi node

--- filter.sh
+++ filter.sh
@@ -6,25 +6,27 @@ echo "Example: ./filter.sh my_experiment 2"
 exit 1
 fi
 
-make
+echo "\033[92mCompilation...\033[0m"
+make > /dev/null
+echo "\033[95mTests ended\033[0m"
 nodes=""
 for i in $(seq 1 $2); do
     j=$((i + 1))
     nodes+="-l strasbourg,m3,$j,build/iotlab/m3/sender.iotlab "
 done
-iotlab-experiment submit -n $1 -d 20 -l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab $nodes
+echo "\033[92mSubmitting experiment...\033[0m"
+iotlab-experiment submit -n $1 -d 20 -l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab $nodes > /dev/null
 iotlab-experiment wait
+echo "\033[92mExperiment start\033[0m"
 
 
 # retrieve node names
 iotlab-experiment get -n | grep "network_address" | sed 's/.*: "\(.*\)".*/\1/' > nodes.txt
 
+echo "\033[92mRetrieving info...\033[0m"
 for node in $(cat nodes.txt)
 do
     # retrieve TSCH & RPL info with netcat during 10s, simplify it and display it
      echo "Node $node :"
     nc $node 20000 | (grep "TSCH" & grep "RPL") &
 done
-
-make clean
-rm nodes.txt
