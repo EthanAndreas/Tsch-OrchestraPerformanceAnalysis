008bde1b2a94ea8be4c5a13031dc9cf7dc1f4822
EthanAndreas <ethanhuret27@gmail.com>
2023-04-27 15:24:28 +0200

    power monitor for coord - ethan & maxime

--- filter.sh
+++ filter.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-echo "Usage: ./filter.sh <experiment_name> <number_of_nodes>"
-echo "Example: ./filter.sh my_experiment 2"
+if [ $# -ne 3 || $# -ne 4 ]; then
+echo "Usage: ./filter.sh <experiment_name> <number_of_nodes> <duration> <monitor>"
+echo "Example: ./filter.sh my_experiment 2 10 power"
+echo "<monitor> is optional, if not specified, no monitoring, else power or radio"
 exit 1
 fi
 
 echo "$(tput setaf 3)Compilation...$(tput setaf 7)"
 make > /dev/null
 echo "$(tput setaf 2)Compiled$(tput setaf 7)"
-nodes=""
+
+if [ $4 -z ]; then
+    nodes="-l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab "
+elif [ $4 = "power" ]; then
+    nodes="-l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab,power_monitor "
+elif [ $4 = "radio" ]; then
+    nodes="-l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab,radio_monitor "
+fi
 for i in $(seq 1 $2); do
     j=$((i + 1))
     nodes+="-l strasbourg,m3,$j,build/iotlab/m3/sender.iotlab "
 done
+
 echo "$(tput setaf 3)Submitting experiment...$(tput setaf 7)"
-iotlab-experiment submit -n $1 -d 20 -l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab $nodes > /dev/null
+iotlab-experiment submit -n $1 -d $3 $nodes > /dev/null
 iotlab-experiment wait 
 echo "$(tput setaf 2)Experiment start$(tput setaf 7)"
 
@@ -32,7 +41,6 @@ do
     # | (grep "TSCH" & grep "RPL") 
 done
 
-make clean > /dev/null
 rm nodes.txt
 iotlab-experiment stop > /dev/null
 echo "$(tput setaf 2)Experiment stop$(tput setaf 7)"
