45d063179f844d6db7e4515248f95afb7e111198
EthanAndreas <ethanhuret27@gmail.com>
2023-04-27 15:07:06 +0200

    multi-node with to - ethan & maxime

--- filter.sh
+++ filter.sh
@@ -6,17 +6,17 @@ echo "Example: ./filter.sh my_experiment 2"
 exit 1
 fi
 
-echo "\033[92mCompilation...\033[0m"
+echo "$(tput setaf 3)Compilation...$(tput setaf 7)"
 make > /dev/null
-echo "$(tput setaf 2) Tests ended $(tput setaf 7)"
+echo "$(tput setaf 2)Compiled$(tput setaf 7)"
 nodes=""
 for i in $(seq 1 $2); do
     j=$((i + 1))
     nodes+="-l strasbourg,m3,$j,build/iotlab/m3/sender.iotlab "
 done
-echo "$(tput setaf 2)Submitting experiment...$(tput setaf 7)"
+echo "$(tput setaf 3)Submitting experiment...$(tput setaf 7)"
 iotlab-experiment submit -n $1 -d 20 -l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab $nodes > /dev/null
-iotlab-experiment wait
+iotlab-experiment wait 
 echo "$(tput setaf 2)Experiment start$(tput setaf 7)"
 
 
@@ -28,5 +28,10 @@ for node in $(cat nodes.txt)
 do
     # retrieve TSCH & RPL info with netcat during 10s, simplify it and display it
     echo "Node $node :"
-    timeout 10 nc $node 20000 | (grep "TSCH" & grep "RPL") 
+    timeout 10 nc $node 20000 
+    # | (grep "TSCH" & grep "RPL") 
 done
+
+make clean > /dev/null
+rm nodes.txt
+iotlab-experiment stop
