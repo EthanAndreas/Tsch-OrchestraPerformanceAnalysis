16db84cb2dfd3f870621495eb5702c90a8e3a0d8
EthanAndreas <ethanhuret27@gmail.com>
2023-04-27 15:02:00 +0200

    retrieve without multi thread - Ethan Maxime

--- filter.sh
+++ filter.sh
@@ -8,25 +8,25 @@ fi
 
 echo "\033[92mCompilation...\033[0m"
 make > /dev/null
-echo "\033[95mTests ended\033[0m"
+echo "$(tput setaf 2) Tests ended $(tput setaf 7)"
 nodes=""
 for i in $(seq 1 $2); do
     j=$((i + 1))
     nodes+="-l strasbourg,m3,$j,build/iotlab/m3/sender.iotlab "
 done
-echo "\033[92mSubmitting experiment...\033[0m"
+echo "$(tput setaf 2)Submitting experiment...$(tput setaf 7)"
 iotlab-experiment submit -n $1 -d 20 -l strasbourg,m3,1,build/iotlab/m3/coordinator.iotlab $nodes > /dev/null
 iotlab-experiment wait
-echo "\033[92mExperiment start\033[0m"
+echo "$(tput setaf 2)Experiment start$(tput setaf 7)"
 
 
 # retrieve node names
 iotlab-experiment get -n | grep "network_address" | sed 's/.*: "\(.*\)".*/\1/' > nodes.txt
 
-echo "\033[92mRetrieving info...\033[0m"
+echo "$(tput setaf 2)Retrieving info...$(tput setaf 7)"
 for node in $(cat nodes.txt)
 do
     # retrieve TSCH & RPL info with netcat during 10s, simplify it and display it
-     echo "Node $node :"
-    nc $node 20000 | (grep "TSCH" & grep "RPL") &
+    echo "Node $node :"
+    timeout 10 nc $node 20000 | (grep "TSCH" & grep "RPL") 
 done
