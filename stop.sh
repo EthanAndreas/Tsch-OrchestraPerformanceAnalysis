a33cbf5928c83ba7e73c6244eeb80fd2d04e7759
EthanAndreas <ethanhuret27@gmail.com>
2023-04-30 00:49:09 +0200

    only one cancel

--- stop.sh
+++ stop.sh
@@ -3,7 +3,7 @@
 if [ $# -gt 1 ]; then
 echo "Usage: ./stop.sh <id>"
 echo "Example: ./stop.sh 123456"
-echo "If no id is given, all experiments are stopped"
+echo "If no id is given, last experiment is stopped"
 exit 1
 fi
 
@@ -15,13 +15,7 @@ if [ $# -eq 1 ]; then
     echo "$(tput setaf 2)Experiment stop$(tput setaf 7)"
     exit 0
 else 
-    # Get all experiments in state Running
-    iotlab-experiment list -s Running | grep "Running" | cut -d"|" -f2 > ids.txt
-    # Stop all experiments
-    for id in $(cat ids.txt); do
-        iotlab-experiment stop -i $id > /dev/null 2>&1
-    done
-    rm ids.txt > /dev/null 2>&1
+    iotlab-experiment stop > /dev/null 2>&1
     echo "$(tput setaf 2)All experiments stopped$(tput setaf 7)"
     exit 0
 fi
