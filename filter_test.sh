ac8a337ea2d688e2a40a453157e9c02a00bd9e2e
Wifi2023stras10 Wifi2023stras10 <wifi2023stras10@srvssh>
2023-04-29 19:33:31 +0200

    fix on monitor

--- filter_test.sh
+++ filter_test.sh
@@ -6,12 +6,15 @@
 iotlab-experiment get -n | grep "network_address" | sed 's/.*: "\(.*\)".*/\1/' > nodes.txt
 
 echo "$(tput setaf 2)Retrieving info...$(tput setaf 7)"
+rm -rf .terminal_out/
+mkdir .terminal_out/
+sleep 3
 for node in $(cat nodes.txt)
 do
     # retrieve TSCH & RPL info with netcat during 10s, simplify it and display it
     echo "$(tput setaf 3)Node $node :$(tput setaf 7)"
     # TODO: remove timeout and replace by thread, one thread per node that execute nc
-    timeout 10 nc $node 20000 | (grep "TSCH" & grep "RPL") 
+    (nc $node 20000 | (grep "TSCH" & grep "RPL"))&
 done
 
 rm nodes.txt > /dev/null 2>&1
--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,7 @@ echo "Usage: ./monitor.sh <experiment_name> <duration> <nodes_number> <site> <mo
 echo "Example: ./monitor.sh my_experiment 10 2 strasbourg power"
 echo "<monitor> : power or radio"
 exit 1
-fi  
+fi
 
 echo "$(tput setaf 3)Compilation...$(tput setaf 7)"
 make > /dev/null 2>&1
@@ -19,33 +19,36 @@ fi
 
 nodes=""
 
-if [ $5 = "power" ]; then
+if [ $5 == "power" ]; then
     iotlab-profile addm3 -n power_monitor -voltage -current -power -period 8244 -avg 4 > /dev/null 2>&1
-    nodes+="-l $4,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,power_monitor "
-elif [ $5 = "radio" ]; then
+    nodes="-l $4,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,power_monitor "
+elif [ $5 == "radio" ]; then
     iotlab-profile addm3 -n radio_monitor -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null 2>&1
-    nodes+="-l $4,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,radio_monitor "
+    nodes="-l $4,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,radio_monitor "
+else
+    echo "$(tput setaf 1)Please enter a monitoring type$(tput setaf 7)"
+    exit 1
 fi
 
 for i in $(seq 1 $(($3 - 1))); do
-    node_id=$(cat nodes_free.txt | tail -n +$((i + 1)) | head -n 1) 
+    node_id=$(cat nodes_free.txt | tail -n +$((i + 1)) | head -n 1)
     nodes+=", -l $4,m3,$node_id,build/iotlab/m3/sender.iotlab"
 done
 
 echo "$(tput setaf 3)Submitting experiment...$(tput setaf 7)"
 iotlab-experiment submit -n $1 -d $2 $nodes > /dev/null 2>&1
-iotlab-experiment wait 
+iotlab-experiment wait
 echo "$(tput setaf 2)Experiment start$(tput setaf 7)"
 
-if [ $5 = "power" ]; then
+if [ $5 == "power" ]; then
     echo "$(tput setaf 3)Retrieving power info...$(tput setaf 7)"
     # wait for file to be created
-    while [ ! -f /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_$(cat nodes_free.txt | head -n 1).oml ]; do
+    while [ ! -f /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_1.oml ]; do
         sleep 1
     done
     # check if the experiment entered at least 100  values in the file
-    line_count=$(wc -l < /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_$(cat nodes_free.txt | head -n 1).oml)
-    while [ $line_count -lt 100 ]; do   
+    line_count=$(wc -l < /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_1.oml)
+    while [ $line_count -lt 100 ]; do
         line_count=$(wc -l < /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_1.oml)
     done
     python3 monitor.py power
@@ -56,7 +59,7 @@ elif [ $5 = "radio" ]; then
     done
     # check if the experiment entered at least 100 values in the file
     line_count=$(wc -l < /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_$(cat nodes_free.txt | head -n 1).oml)
-    while [ $line_count -lt 100 ]; do   
+    while [ $line_count -lt 100 ]; do
         line_count=$(wc -l < /senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_$(cat nodes_free.txt | head -n 1).oml)
     done
     python3 monitor.py radio
