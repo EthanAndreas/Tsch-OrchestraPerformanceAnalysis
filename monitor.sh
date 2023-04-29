8d38387f5d854703a9be6d36186af3dc9be9d15c
EthanAndreas <ethanhuret27@gmail.com>
2023-04-29 20:24:49 +0200

    strasbourg site condition

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 5 ]; then
-echo "Usage: ./monitor.sh <experiment_name> <duration> <nodes_number> <site> <monitor>"
+if [ $# -ne 4 ]; then
+echo "Usage: ./monitor.sh <experiment_name> <duration> <nodes_number> <monitor>"
 echo "Example: ./monitor.sh my_experiment 10 2 strasbourg power"
 echo "<monitor> : power or radio"
+echo "PS: Result of experiment is only accessible for Strasbourg site"
 exit 1
 fi
 
@@ -11,18 +12,18 @@ echo "$(tput setaf 3)Compilation...$(tput setaf 7)"
 make > /dev/null 2>&1
 echo "$(tput setaf 2)Compiled$(tput setaf 7)"
 
-iotlab-status --nodes --archi m3 --state Alive --site $4 | grep network |cut -d"-" -f2 |cut -d"." -f1 > nodes_free.txt
+iotlab-status --nodes --archi m3 --state Alive --site strasbourg | grep network |cut -d"-" -f2 |cut -d"." -f1 > nodes_free.txt
 if [ $(cat nodes_free.txt | wc -l) -lt $3 ]; then
     echo "$(tput setaf 1)Not enough nodes available$(tput setaf 7)"
     exit 1
 fi
 
-if [ $5 == "power" ]; then
+if [ $4 == "power" ]; then
     iotlab-profile addm3 -n power_monitor -voltage -current -power -period 8244 -avg 4 > /dev/null 2>&1
-    nodes="-l $4,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,power_monitor"
-elif [ $5 == "radio" ]; then
+    nodes="-l strasbourg,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,power_monitor"
+elif [ $4 == "radio" ]; then
     iotlab-profile addm3 -n radio_monitor -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null 2>&1
-    nodes="-l $4,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,radio_monitor"
+    nodes="-l strasbourg,m3,$(cat nodes_free.txt | head -n 1),build/iotlab/m3/coordinator.iotlab,radio_monitor"
 else
     echo "$(tput setaf 1)Please enter a monitoring type$(tput setaf 7)"
     exit 1
@@ -30,10 +31,10 @@ fi
 
 for i in $(seq 1 $(($3 - 1))); do
     node_id=$(cat nodes_free.txt | tail -n +$((i + 1)) | head -n 1)
-    if [ $5 == "power" ]; then
-        nodes+=" -l $4,m3,$node_id,build/iotlab/m3/sender.iotlab,power_monitor"
-    elif [ $5 == "radio" ]; then
-        nodes+=" -l $4,m3,$node_id,build/iotlab/m3/sender.iotlab,radio_monitor"
+    if [ $4 == "power" ]; then
+        nodes+=" -l strasbourg,m3,$node_id,build/iotlab/m3/sender.iotlab,power_monitor"
+    elif [ $4 == "radio" ]; then
+        nodes+=" -l strasbourg,m3,$node_id,build/iotlab/m3/sender.iotlab,radio_monitor"
     fi
 done
 
@@ -43,7 +44,7 @@ echo "Experiment ID : $id"
 iotlab-experiment wait -i $id
 echo "$(tput setaf 2)Experiment start$(tput setaf 7)"
 
-if [ $5 == "power" ]; then
+if [ $4 == "power" ]; then
     echo "$(tput setaf 3)Retrieving power info...$(tput setaf 7)"
     file="/senslab/users/wifi2023stras10/.iot-lab/last/consumption/m3_1.oml"
     # wait for file to be created
@@ -56,7 +57,7 @@ if [ $5 == "power" ]; then
         line_count=$(wc -l < file)
     done
     python3 monitor.py power
-elif [ $5 == "radio" ]; then
+elif [ $4 == "radio" ]; then
     echo "$(tput setaf 3)Retrieving radio info...$(tput setaf 7)"
     file="/senslab/users/wifi2023stras10/.iot-lab/last/radio/m3_1.oml"
     while [ ! -f file ]; do
