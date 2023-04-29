1dd8a2f3ec68ebf6d0e3c6d91c287e4710a7876d
EthanAndreas <ethanhuret27@gmail.com>
2023-04-30 00:38:55 +0200

    func to accelerate manip

--- check_free_node.sh
+++ check_free_node.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-iotlab-status --nodes --archi m3 --state Alive --site strasbourg | grep network  | cut -d ":" -f2 | tr -d '",'
+if [ $# -ne 1 ]; then
+echo "Usage: ./check_free_node.sh <site>"
+echo "Example: ./check_free_node.sh strasbourg"
+exit 1
+fi
+
+iotlab-status --nodes --archi m3 --state Alive --site $1 | grep network  | cut -d ":" -f2 | tr -d '",'
--- stop.sh
+++ stop.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+if [ $# -gt 1 ]; then
+echo "Usage: ./stop.sh <id>"
+echo "Example: ./stop.sh 123456"
+echo "If no id is given, all experiments are stopped"
+exit 1
+fi
+
 echo "$(tput setaf 3)Clean...$(tput setaf 7)"
 rm *.txt > /dev/null 2>&1
 echo "$(tput setaf 2)Cleaned$(tput setaf 7)"
-iotlab-experiment stop > /dev/null 2>&1
-echo "$(tput setaf 2)Experiment stop$(tput setaf 7)"
+if [ $# -eq 1 ]; then
+    iotlab-experiment stop -i $1 > /dev/null 2>&1
+    echo "$(tput setaf 2)Experiment stop$(tput setaf 7)"
+    exit 0
+else 
+    iotlab-experiment stop -l > /dev/null 2>&1
+    echo "$(tput setaf 2)All experiments stopped$(tput setaf 7)"
+    exit 0
+fi
