2e5291cb20db3d41810e98080a2090697d847b19
Wifi2023stras10 Wifi2023stras10 <wifi2023stras10@srvssh>
2023-04-30 15:06:17 +0200

    filter

--- filter.sh
+++ filter.sh
@@ -3,28 +3,27 @@ function write_terminal(){
 	echo "$(tput setaf 2)Process $1 terminated$(tput setaf 7)"
 }
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
 echo "Usage: ./filter.sh <experiment_name> <duration> <number_of_nodes> <site>"
 echo "Example: ./filter.sh my_experiment 10 2 strasbourg"
 exit 1
 fi
 
-./submit.sh $1 $2 $3 $4
+./submit.sh $1 $2 $3 $4 $5
 
 # retrieve node names
 iotlab-experiment get -n | grep "network_address" | sed 's/.*: "\(.*\)".*/\1/' > nodes.txt
 
-echo "$(tput setaf 2)Retrieving info...$(tput setaf 7)"
-mkdir netcat/
-rm netcat/* > /dev/null 2>&1
+echo "$(tput setaf 3)Retrieving info...$(tput setaf 7)"
+rm -rf netcat > /dev/null 2>&1
+mkdir netcat > /dev/null 2>&1
 sleep 3
 
 for node in $(cat nodes.txt)
 do
     # retrieve TSCH & RPL info with netcat of each node, simplify it and display it
     echo "$(tput setaf 3)Node $node :$(tput setaf 7)"
-   (write_terminal $node)&
+    (write_terminal $node)&
 done
 
-rm -rf netcat/ > /dev/null 2>&1
 rm nodes.txt > /dev/null 2>&1
